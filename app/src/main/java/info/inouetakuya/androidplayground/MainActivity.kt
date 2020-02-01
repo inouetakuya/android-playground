48b942b7d158d8b7a21eaac1e0d1ed3737809107
INOUE Takuya <inouetakuya5@gmail.com>
2020-02-02 00:02:12 +0900

    8 文字の色を変えた

--- app/src/main/java/info/inouetakuya/androidplayground/MainActivity.kt
+++ app/src/main/java/info/inouetakuya/androidplayground/MainActivity.kt
@@ -1,8 +1,8 @@
 package info.inouetakuya.androidplayground
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
@@ -13,9 +13,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         getOmikujiButton.setOnClickListener {
-            Log.v("MainActivity", "Button clicked")
+            // Log.v("MainActivity", "Button clicked")
             val results = arrayOf("大吉", "中吉", "吉", "小吉", "凶")
             val n = Random.nextInt(results.count())
+            if (n == 0) {
+                // resultTextView.setTextColor(Color.RED)
+                // resultTextView.setTextColor(Color.parseColor("#ff0000"))
+                resultTextView.setTextColor(Color.argb(255, 255, 0, 0))
+            } else {
+                resultTextView.setTextColor(Color.parseColor("#000000"))
+            }
             resultTextView.text = results[n]
         }
     }
